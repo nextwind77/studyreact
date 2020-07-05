6007da272cd7b21a3358538d7b66e005bcf122d8
jinho <>
2020-07-06 02:50:44 +0900

    Updates

---
+++ precache-manifest.acd23ba34840f76d8c6358fec311317c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33675017e63cda0f28a31a5295eab2a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e03631bcb22414a549",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1188c8c33aadcc65d76",
+    "url": "/static/js/2.de28381b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de28381b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e03631bcb22414a549",
+    "url": "/static/js/main.0ccde111.chunk.js"
+  },
+  {
+    "revision": "eeb9f3f5acfc99e8ff43",
+    "url": "/static/js/runtime-main.3af7429c.js"
+  }
+]);
